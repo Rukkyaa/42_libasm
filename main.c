5c8787f2543cc826dda3ca1d9b2637b929837f75
Rukkyaa <lbrt.axel@gmail.com>
2023-07-17 22:41:34 +0200

    feat: ft_strcmp works now

--- main.c
+++ main.c
@@ -3,13 +3,10 @@
 #include <stdio.h>
 
 int main() {
-    char *source = "Hello World !";
-    char *destination = "Hello";
+    char *source = "Chichi";
+    char *destination = "zhichi";
 
-    // ft_strcpy(destination, source);
-    printf("Res: %d\n", ft_strcmp(source, destination));
+    printf("My res: %d\n", ft_strcmp(source, destination));
     printf("Res: %d\n", strcmp(source, destination));
-    // printf("La chaîne de caractères copiée est : %s\n", destination);
-
     return 0;
 }
--- srcs/ft_strcmp.s
+++ srcs/ft_strcmp.s
@@ -8,22 +8,20 @@ section .text
 	global ft_strcmp
 
 ft_strcmp:
-	xor rcx, rcx		;set rcx to -1 to start at 0
-	jmp loop_ft_strcmp
-
-loop_ft_strcmp:
-	inc rcx							;increment the counter
-	mov al, [rdi + rcx]				;move the current char of s1 to al
-	cmp al, 0						;check if s1 is null
-	je end_ft_strcmp				;if it is, return 0
-	mov bl, [rsi + rcx]				;move the current char of s2 to bl
-	cmp bl, 0						;check if s2 is null
-	je end_ft_strcmp				;if it is, return 0
-	cmp al, bl						;compare the current char of s1 and s2
-	je loop_ft_strcmp				;if they are equal, continue
-	jmp end_ft_strcmp				;if they are not, return the difference
+	mov al, [rdi]		;move *s1 to al
+	mov bl, [rsi]		;move *s2 to bl
+	cmp al, 0			;check if *s1 is null
+	je end_ft_strcmp	;if it is, go to end_ft_strcmp
+	cmp bl, 0			;check if s2 is null
+	je end_ft_strcmp	;if it is, go to end_ft_strcmp
+	cmp al, bl			;compare *s1 and *s2
+	jne end_ft_strcmp	;if they are not equal, go to end_ft_strcmp
+	inc rdi				;increment *s1
+	inc rsi				;increment *s2
+	jmp ft_strcmp		;if they are equal, continue
 
 end_ft_strcmp:
-	mov rax, [rdi + rcx]
-	sub rax, [rsi + rcx]
+	movzx rax, al	;rax = the ascii value of *s1
+	movzx r8, bl	;register to stock tmp data
+	sub rax, r8		;rax -= *s2, to check the value of the return
 	ret
