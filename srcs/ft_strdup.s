207252078639cdf6299142f8a1dfa26407b828bc
Rukkyaa <lbrt.axel@gmail.com>
2023-07-19 13:58:07 +0200

    feat: strdup final done

--- srcs/ft_strdup.s
+++ srcs/ft_strdup.s
@@ -15,9 +15,9 @@ ft_strdup:
 	call ft_strlen          ;rax = ft_strlen(rdi)
 	inc rax                 ;increment rax to include '\0'
 	push rdi                ;push rdi to the stack
-	mov rdi, rax			;rdi = rax 
-	call malloc wrt ..plt	;call malloc with length of string(rax)
-	mov rdi, rax			;moving dst's string to rdi
-	pop rsi					;moving src's string to rsi
-	call ft_strcpy			;call ft_strcpy(rdi, rsi)
+	mov rdi, rax            ;rdi = rax 
+	call malloc wrt ..plt   ;call malloc with length of string(rax)
+	mov rdi, rax            ;moving dst's string to rdi
+	pop rsi                 ;moving src's string to rsi
+	call ft_strcpy          ;call ft_strcpy(rdi, rsi)
 	ret
